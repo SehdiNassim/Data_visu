6d288a9024bf7f85108ee76859aef8478b4e5a58
Sehdi Nassim <hm_sehdi@esi.dz>
2022-03-28 18:37:10 +0200

    Added preprocessing section

--- prog_web.py
+++ prog_web.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 import os
-
 
 # Global variables
 dummification, outliers, valeurs_manquantes, normalisation = [False for i in range(4)]
@@ -13,7 +13,7 @@ st.title("Bienvenue")
 
 siteHeader = st.container()
 dataExploration = st.container()
-newFeatures = st.container()
+preprocessing = st.container()
 modelTraining = st.container()
 
 with siteHeader:
@@ -53,10 +53,11 @@ with dataExploration:
         st.subheader("Analyse Exploratoire Univariée sur variable qualitative")
         col1, col2 = st.columns([1, 3])
         current_column2 = col1.radio("Veuillez selectionner un feature", newdf2.columns)
-        col2.table(newdf2[current_column2].value_counts())
+        # col2.table(newdf2[current_column2].value_counts())
         fig2, ax2 = plt.subplots()
         ax2.set_title('diagramme circulaire de la variable {}'.format(current_column2))
-        ax2.pie(newdf2[current_column2].value_counts().values,labels=newdf2[current_column2].value_counts().index)
+        value_counts=newdf2[current_column2].value_counts()
+        ax2.pie(value_counts.values,labels=value_counts.index)
         col2.pyplot(fig2)
         # Analyse bidimensionnelle
         st.subheader("Analyse Exploratoire Bivariée")
@@ -69,15 +70,28 @@ with dataExploration:
         ax3.set_title("Matrice de corrélation \n")
         sns.heatmap(corr,cmap="Blues",annot=True, xticklabels=corr.columns, yticklabels=corr.columns, ax=ax3)
         st.pyplot(fig3)
-
-        st.write('Veuillez choisir vos parametres:')
+    except:
+        st.warning('No file yet to explore')
+with preprocessing:
+    try:
+        # Preprocessing
+        st.write('Preprocessing:')
+        st.write('Quel est votre outcome ?')
+        output_var = st.selectbox('', df.columns)
+        df[output_var+'_ouput']= LabelEncoder().fit_transform(df[[output_var]])
+        droped_vars = st.multiselect('Quelles colonnes voulez vous supprimer ?', df.columns)
+        df = df.drop(droped_vars, axis=1)
+        st.write('Quels prétraitements souhaitez vous appliquer à vos features ?')
         col1, col2 = st.columns(2)
         dummification = col1.checkbox('Dummification')
         outliers = col1.checkbox('Outliers')
         valeurs_manquantes = col2.checkbox('Valeurs manquante')
         normalisation = col2.checkbox('Normalisation')
-        if dummification: df = pd.get_dummies(df)
+        try:
+            if dummification: df = pd.get_dummies(df, columns=newdf2.drop(output_var, axis=1).columns)
+        except KeyError:
+            if dummification: df = pd.get_dummies(df, columns=newdf2.columns)
         if valeurs_manquantes: df = df.dropna()
         st.table(df.head())
     except:
-        st.warning('No file yet to explore')
+        st.warning('An error occured during preprocessing')
